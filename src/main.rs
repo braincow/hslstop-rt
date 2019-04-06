1c96812b2a8b3e996bf2398e12759017493a4925
Antti Peltonen <antti.peltonen@iki.fi>
2019-04-06 11:00:31 +0300

    graphql response now parsed into prettytable

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ extern crate graphql_client;
 extern crate failure;
 extern crate reqwest;
 extern crate dotenv;
+#[macro_use]
+extern crate prettytable;
+extern crate chrono;
 
 use dotenv::dotenv;
 use std::env;
 use graphql_client::{GraphQLQuery, Response};
+use prettytable::Table;
+use chrono::{TimeZone, Utc, Local};
 
 // schema contains scalar Long that Rust language has no analog for so we map it to f64
 type Long = f64;
@@ -36,7 +41,36 @@ fn perform_my_query(variables: stops_query::Variables) -> Result<Response<stops_
 fn main() -> Result<(), failure::Error> {
     dotenv().ok();
     let response = perform_my_query(stops_query::Variables { name: Some(env::var("STOP_NAME").unwrap()) }).unwrap();
-    println!("{:#?}", response);
+    //println!("{:#?}", response);
+    let mut table = Table::new();
+    for stop in response.data.expect("no response data").stops.expect("no stops in response")
+    {
+        if let Some(stop) = stop {
+            //println!("{:#?}", stop);
+            for time in stop.stoptimes_without_patterns.expect("no stop times in response") {
+                if let Some(time) = time {
+                    let service_day_seconds = time.service_day.expect("no service day for stop time");
+                    let trip = time.trip.expect("no trip info for stop time");
+                    let realtime = time.realtime.expect("no realtime flag for stop time");
+                    let departure_seconds;
+                    if realtime == true {
+                        departure_seconds = time.realtime_departure.expect("no realtime timestamp in stop time");
+                    } else {
+                        departure_seconds = time.scheduled_departure.expect("no scheduled timestamp in stop time");
+                    }
+                    let utc_datetime = Utc.timestamp(service_day_seconds as i64 + departure_seconds, 0);
+                    let departure_datetime = utc_datetime.with_timezone(&Local);
+                    table.add_row(row!(
+                        trip.route_short_name.expect("no route short name for trip"),
+                        trip.trip_headsign.expect("no headsign for trip"),
+                        realtime,
+                        departure_datetime
+                    ));
+                }
+            }
+        }
+    }
+    table.printstd();
     Ok(())
 }
 // eof
